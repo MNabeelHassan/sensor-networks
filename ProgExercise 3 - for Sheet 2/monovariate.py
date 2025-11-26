0196035f194a2c39675812d5a1263cec449d1f9f
MNabeelHassan <nabeelhassan1295@gmail.com>
2025-11-26 23:25:36 +0100

    Solves monovariate

--- ProgExercise 3 - for Sheet 2/monovariate.py
+++ ProgExercise 3 - for Sheet 2/monovariate.py
@@ -7,19 +7,34 @@ import scipy.stats as stats
 # FILL THIS FUNCTION
 def mean(values):
     mu = 0
-    # YOUR CODE HERE
+    sum_of_values = 0
+    no_of_values = len(values)
+    for value in values:
+        sum_of_values += value
+    mu = sum_of_values / no_of_values
     return mu
 
 # FILL THIS FUNCTION
 def variance(values):
     sigma2 = 0
-    # YOUR CODE HERE
+    len_minus_one = len(values) - 1
+    mu = mean(values)
+    deviation = []
+    squared_deviation = []
+    for value in values:
+        deviation.append(value - mu)
+    for dev in deviation:
+        squared_deviation.append(dev ** 2)
+    sum_of_squared_deviation = 0
+    for sq_dev in squared_deviation:
+        sum_of_squared_deviation += sq_dev
+    sigma2 = sum_of_squared_deviation / len_minus_one
     return sigma2
     
 # Create a 10-size distribution between two bounds
-values = []
-for i in range(10):
-    values.append(randint(-5, 20))
+values = [-2, 15, 4, 2, 16, 4, 3, -4, 15, 7]
+# for i in range(10):
+#     values.append(randint(-5, 20))
     
 # Compute the mean and variance
 mu = mean(values)
